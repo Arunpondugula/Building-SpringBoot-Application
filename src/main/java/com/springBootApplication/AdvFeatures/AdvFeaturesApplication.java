7be3e6069a708235548c908e6829694d2d503667
Arunpondugula <arunpondugula@gmail.com>
2023-10-07 17:36:25 -0500

    Initial commit before starting the project

---
+++ src/main/java/com/springBootApplication/AdvFeatures/AdvFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.springBootApplication.AdvFeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvFeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBootApplication/AdvFeatures/AdvFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootApplication.AdvFeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
